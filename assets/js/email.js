efdd119bf318f3380bbf2d64dbcc1df68e0314eb
realgitau <antonygitau04@gmail.com>
2023-03-12 13:50:48 +0300

    second commit

---
+++ assets/js/email.js
@@ -0,0 +1,21 @@
+// const name = document.getElementById('name').value;
+// const mail = document.getElementById('email').value;
+// const message = document.getElementById('message').value;
+// const button = document.getElementById('form-submit');
+
+// function SendMail(){
+//     var Params = {
+//         from_name: document.getElementById('name').value,
+//          email: document.getElementById('email').value,
+//          message: document.getElementById('message').value
+//      };
+      
+//      emailjs.send('service_the1wkv', 'template_wgkna9p', Params)
+//          .then(function(response) {
+//             alert('SUCCESS!' + response.status);
+//          }, function(error) {
+//             alert('FAILED...' + error);
+//          });
+// }
+
+
