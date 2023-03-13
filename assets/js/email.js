b7c48381c79d58d7c5a68718ac4283fa24ebb960
realgitau <antonygitau04@gmail.com>
2023-03-13 16:29:07 +0300

    emailjs

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,21 +1,19 @@
-// const name = document.getElementById('name').value;
-// const mail = document.getElementById('email').value;
-// const message = document.getElementById('message').value;
-// const button = document.getElementById('form-submit');
 
-// function SendMail(){
-//     var Params = {
-//         from_name: document.getElementById('name').value,
-//          email: document.getElementById('email').value,
-//          message: document.getElementById('message').value
-//      };
+const button = document.getElementById('form-submit');
+
+function SendMail(){
+    var Params = {
+        name: document.getElementById('name').value,
+         email: document.getElementById('email').value,
+         message: document.getElementById('message').value
+     };
       
-//      emailjs.send('service_the1wkv', 'template_wgkna9p', Params)
-//          .then(function(response) {
-//             alert('SUCCESS!' + response.status);
-//          }, function(error) {
-//             alert('FAILED...' + error);
-//          });
-// }
+     emailjs.send('service_the1wkv', 'template_wgkna9p', Params)
+         .then(function(response) {
+            console.log('SUCCESS!' , response.status, response.text);
+         }, function(error) {
+            console.log('FAILED...' , error);
+         });
+}
 
 
