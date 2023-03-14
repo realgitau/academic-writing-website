e9cfbfb4e121d3c599b2a7fecd928a7aec9d19fb
hillaryke <tenskap@gmail.com>
2023-03-14 14:10:35 +0300

    Fix sending email with emailjs

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,17 +1,6 @@
 
-const button = document.getElementById('form-submit');
-
 function SendMail(){
-    // alert('hello')
-    var Params = {
-        name: document.getElementById('name').value,
-         email: document.getElementById('email').value,
-         message: document.getElementById('message').value
-     };
-    //  alert(Params.name)
-     alert( Params.email)
-      
-     emailjs.sendForm('service_the1wkv1', 'template_wgkna9p', "#contact-form", "Rig9WDk0WVxg8HpVS")
+     emailjs.sendForm('service_the1wkv1', 'template_wgkna9p', "#contact-form")
          .then(function(response) {
             console.log('SUCCESS!' , response.status, response.text);
          }, function(error) {
