4a5c1bfcfc72ce752f844eb079eb9fe056464b28
realgitau <antonygitau04@gmail.com>
2023-03-14 13:40:24 +0300

    another one

--- assets/js/email.js
+++ assets/js/email.js
@@ -2,13 +2,16 @@
 const button = document.getElementById('form-submit');
 
 function SendMail(){
+    // alert('hello')
     var Params = {
         name: document.getElementById('name').value,
          email: document.getElementById('email').value,
          message: document.getElementById('message').value
      };
+    //  alert(Params.name)
+     alert( Params.email)
       
-     emailjs.send('service_the1wkv', 'template_wgkna9p', Params)
+     emailjs.sendForm('service_the1wkv1', 'template_wgkna9p', "#contact-form", "Rig9WDk0WVxg8HpVS")
          .then(function(response) {
             console.log('SUCCESS!' , response.status, response.text);
          }, function(error) {
